12e257cb554fa2757d18f8f338d24c95e9ddc056
gileadekelvin <gileadekelvin@gmail.com>
2020-10-15 10:53:57 -0300

    Adiciona módulo de relatorias Recupera relatores das proposições monitoradas pelo Leggo

--- code/export_data.R
+++ code/export_data.R
@@ -5,5 +5,6 @@ message("Recupera, processa e exporta dados de parlamentares, proposições e tw
 source(here::here("code/parlamentares/export_parlamentares.R"))
 source(here::here("code/proposicoes/export_proposicoes.R"))
 source(here::here("code/tweets/export_tweets.R"))
+source(here::here("code/relatorias/export_relatorias.R"))
 
 message("Concluído!")
---
+++ code/relatorias/fetcher_relatorias.R
@@ -0,0 +1,27 @@
+#' @title Recupera as relatorias das proposições de uma agenda
+#' @description Recupera todos os relatores das proposições de uma agenda
+#' @param agenda Agenda de interesse (obs: sem acentos e espaços,
+#' ex: reforma-tributaria)
+#' @return Dataframe com informações dos relatores das proposições de uma agenda.
+fetch_relatores_proposicoes_by_agenda <-
+  function(agenda = "congresso-remoto") {
+    library(tidyverse)
+    
+    print(paste0("Baixando Relatores de proposições da agenda ", agenda, "..."))
+    
+    url <-
+      paste0("http://agorapi:8000/relatores/?interesse=",
+             agenda)
+    
+    df <-
+      RCurl::getURL(url, .encoding = "UTF-8") %>%
+      jsonlite::fromJSON()
+    
+    relatores <- df %>%
+      select(id_parlamentar_parlametria = relator_id_parlametria,
+             id_proposicao = id_leggo,
+             casa) %>% 
+      distinct()
+    
+    return(relatores)
+  }
