08d5c088f790627f7d7aef5f17aeae80e6031dea
Hadrizia Santos <hadrizia.santos@ccc.ufcg.edu.br>
2020-11-09 15:27:31 -0300

    Adiciona migration que retira coluna outrage

--- feed/manage.py
+++ feed/manage.py
@@ -67,6 +67,7 @@ def do_migrations():
     execute_migration('/code/scripts/migrations/0001_tweet_proposicao.sql')
     execute_migration('/code/scripts/migrations/0002_tema.sql')
     execute_migration('/code/scripts/migrations/0003_tweet.sql')
+    execute_migration('/code/scripts/migrations/0004_tweet.sql')
 
 cli.add_command(shell)
 cli.add_command(create_tables)
