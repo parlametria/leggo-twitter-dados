0f53428e8e3eb74c089be38fad06a453462277ff
Hadrizia Santos <hadrizia.santos@ccc.ufcg.edu.br>
2020-12-14 17:46:33 -0300

    Adiciona opção de gerar dados de tweets sobre proposições vindas de csv

--- code/parlamentares/export_parlamentares.R
+++ code/parlamentares/export_parlamentares.R
@@ -14,7 +14,10 @@ option_list = list(
   make_option(c("-o", "--out"), type="character", default=here::here("data/parlamentares/parlamentares.csv"), 
               help="nome do arquivo de saída [default= %default]", metavar="character"),
   make_option(c("-u", "--url"), type="character", default="https://dev.api.leggo.org.br", 
-              help="url da api do parlametria [default= %default]", metavar="character")
+              help="url da api do parlametria [default= %default]", metavar="character"),
+  make_option(c("-p", "--prop"), type="character", default=NULL, 
+              help="caminho para o csv de proposições [default= %default]")
+  
 ) 
 
 opt_parser = OptionParser(option_list=option_list)
