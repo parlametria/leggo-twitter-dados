a379fb37874302c81494da40f685a6211fd9c3b1
Hadrizia Santos <hadrizia.santos@ccc.ufcg.edu.br>
2021-05-27 12:48:11 -0300

    Adiciona arg de url da api

--- code/usuarios/export_usuarios.R
+++ code/usuarios/export_usuarios.R
@@ -12,7 +12,9 @@ message("Use --help para mais informações\n")
 
 option_list = list(
   make_option(c("-o", "--out"), type="character", default=here::here("data/usuarios/usuarios.csv"), 
-              help="nome do arquivo de saída [default= %default]", metavar="character")
+              help="nome do arquivo de saída [default= %default]", metavar="character"),
+  make_option(c("-u", "--url"), type="character", default="https://dev.api.leggo.org.br", 
+              help="url da api do parlametria [default= %default]", metavar="character")
   
 ) 
 
@@ -20,7 +22,6 @@ opt_parser = OptionParser(option_list=option_list)
 opt = parse_args(opt_parser)
 
 saida <- opt$out
-
 
 export_usuarios <- function(saida = here::here("data/usuarios/usuarios.csv")) {
   message("Iniciando processamento de usuários monitorados")
